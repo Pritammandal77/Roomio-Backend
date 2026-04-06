10662ee21a41d2ba573fd8701f091f25db7da212
Pritam Mandal <pritampmandal@gmail.com>
2026-04-06 23:36:41 +0530

    created controller to fetch interests

--- src/controllers/interest.controller.js
+++ src/controllers/interest.controller.js
@@ -1,20 +1,24 @@
 import { Interest } from "../models/interested.model";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { asyncHandler } from "../utils/AsyncHandler.js"
+import { asyncHandler } from "../utils/AsyncHandler";
 
 
 export const createNewInterest = asyncHandler(async (req, res) => {
     const { propertyLister, propertyId, message } = req.body;
 
+    const interestedUser = req.user?._id;
+
+    if (!interestedUser) {
+        throw new ApiError(401, "User not logged in");
+    }
+
     if (!propertyLister || !propertyId) {
         throw new ApiError(400, "Property and lister are required");
     }
 
-    const interestedUser = req.user?._id;
-
-    if (!interestedUser) {
-        throw new ApiError(401, "User not logged In")
+    if (interestedUser.toString() === propertyLister) {
+        throw new ApiError(400, "You cannot show interest in your own property");
     }
 
     try {
@@ -26,7 +30,7 @@ export const createNewInterest = asyncHandler(async (req, res) => {
         });
 
         return res.status(201).json(
-            new ApiResponse(201, interest, "Interest sent")
+            new ApiResponse(201, interest, "Interest sent successfully")
         );
 
     } catch (error) {
@@ -35,4 +39,22 @@ export const createNewInterest = asyncHandler(async (req, res) => {
         }
         throw error;
     }
+});
+
+
+export const getUserInterests = asyncHandler(async (req, res) => {
+    const user = req.user?._id;
+
+    if (!user) {
+        throw new ApiError(401, "User not loggedIn")
+    }
+
+    const incoming = await Interest.find({ propertyLister: user }).sort({ createdAt: -1 })
+    const outgoing = await Interest.find({ interestedUser: user }).sort({ createdAt: -1 })
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, { incoming, outgoing }, "Interests fetched successfully")
+        );
 })
