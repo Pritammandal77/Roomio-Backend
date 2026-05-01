6e6bbd5d0cc8143de92bb440bd8f4ccea8e40c64
Pritam Mandal <pritampmandal@gmail.com>
2026-05-01 10:49:56 +0530

    disabled the OTP verification temp

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -19,91 +19,91 @@ import { Otp } from "../models/otp.model.js";
 // Refresh token stored in DB →
 // Cookies set →
 // User logged in 
-// export const createNewUser = asyncHandler(async (req, res) => {
-//     const { email, password, confirmPassword, fullName, dob, mobileNumber, gender } = req.body;
+export const createNewUser = asyncHandler(async (req, res) => {
+    const { email, password, confirmPassword, fullName, dob, mobileNumber, gender } = req.body;
 
-//     if (!email || !password || !confirmPassword || !fullName || !dob || !mobileNumber || !gender) {
-//         throw new ApiError(400, "All fields are required");
-//     }
+    if (!email || !password || !confirmPassword || !fullName || !dob || !mobileNumber || !gender) {
+        throw new ApiError(400, "All fields are required");
+    }
 
-//     const existedUser = await User.findOne({ email });
+    const existedUser = await User.findOne({ email });
 
-//     if (existedUser) {
-//         throw new ApiError(409, "User with this email already exist");
-//     }
+    if (existedUser) {
+        throw new ApiError(409, "User with this email already exist");
+    }
 
-//     if (password !== confirmPassword) {
-//         throw new ApiError(400, "Passwords do not match");
-//     }
+    if (password !== confirmPassword) {
+        throw new ApiError(400, "Passwords do not match");
+    }
 
-//     const profilePictureLocalPath = req.files?.profilePicture?.[0]?.path;
+    const profilePictureLocalPath = req.files?.profilePicture?.[0]?.path;
 
-//     let profilePictureLiveURL;
-//     if (profilePictureLocalPath) {
-//         const profilePicture = await uploadOnCloudinary(profilePictureLocalPath);
-//         if (!profilePicture?.url) {
-//             throw new ApiError(400, "Error while uploading profile picture");
-//         }
-//         profilePictureLiveURL = profilePicture.url;
-//     }
+    let profilePictureLiveURL;
+    if (profilePictureLocalPath) {
+        const profilePicture = await uploadOnCloudinary(profilePictureLocalPath);
+        if (!profilePicture?.url) {
+            throw new ApiError(400, "Error while uploading profile picture");
+        }
+        profilePictureLiveURL = profilePicture.url;
+    }
 
-//     const user = await User.create(
-//         {
-//             email,
-//             password,
-//             fullName,
-//             dob,
-//             mobileNumber,
-//             gender,
-//             profilePicture: profilePictureLiveURL,
-//             authProvider: "email"
-//         }
-//     )
+    const user = await User.create(
+        {
+            email,
+            password,
+            fullName,
+            dob,
+            mobileNumber,
+            gender,
+            profilePicture: profilePictureLiveURL,
+            authProvider: "email"
+        }
+    )
 
-//     const accessToken = createAccessToken(user);
+    const accessToken = createAccessToken(user);
 
-//     const { token: refreshToken, id: refreshId } = await createRefreshToken({
-//         userId: user._id,
-//         ip: req.ip,
-//         userAgent: req.headers["user-agent"],
-//     });
+    const { token: refreshToken, id: refreshId } = await createRefreshToken({
+        userId: user._id,
+        ip: req.ip,
+        userAgent: req.headers["user-agent"],
+    });
 
-//     const isProd = process.env.NODE_ENV === "production";
+    const isProd = process.env.NODE_ENV === "production";
 
-//     const cookieOptions = {
-//         httpOnly: true,
-//         secure: isProd,
-//         sameSite: isProd ? "None" : "Lax",
-//         path: "/",
-//     };
+    const cookieOptions = {
+        httpOnly: true,
+        secure: isProd,
+        sameSite: isProd ? "None" : "Lax",
+        path: "/",
+    };
 
-//     res.cookie("access_token", accessToken, {
-//         ...cookieOptions,
-//         maxAge: 2 * 60 * 60 * 1000,
-//     });
+    res.cookie("access_token", accessToken, {
+        ...cookieOptions,
+        maxAge: 2 * 60 * 60 * 1000,
+    });
 
-//     res.cookie("refresh_token", refreshToken, {
-//         ...cookieOptions,
-//         maxAge: 30 * 24 * 60 * 60 * 1000,
-//     });
+    res.cookie("refresh_token", refreshToken, {
+        ...cookieOptions,
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
 
-//     res.cookie("refresh_token_id", refreshId.toString(), {
-//         ...cookieOptions,
-//         maxAge: 30 * 24 * 60 * 60 * 1000,
-//     });
+    res.cookie("refresh_token_id", refreshId.toString(), {
+        ...cookieOptions,
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
 
-//     return res
-//         .status(201)
-//         .json(
-//             new ApiResponse(201,
-//                 {
-//                     _id: user._id,
-//                     email: user.email,
-//                     fullName: user.fullName,
-//                 },
-//                 "User registered successfully")
-//         )
-// })
+    return res
+        .status(201)
+        .json(
+            new ApiResponse(201,
+                {
+                    _id: user._id,
+                    email: user.email,
+                    fullName: user.fullName,
+                },
+                "User registered successfully")
+        )
+})
 
 
 export const getCurrentUser = asyncHandler(async (req, res) => {
@@ -301,98 +301,99 @@ export const logOutUser = asyncHandler(async (req, res) => {
 })
 
 
-export const createNewUser = asyncHandler(async (req, res) => {
-    const { email, password, confirmPassword, fullName, dob, mobileNumber, gender } = req.body;
+// With OTP flow : Working conrrectly 
+// export const createNewUser = asyncHandler(async (req, res) => {
+//     const { email, password, confirmPassword, fullName, dob, mobileNumber, gender } = req.body;
 
-    if (!email || !password || !confirmPassword || !fullName || !dob || !mobileNumber || !gender) {
-        throw new ApiError(400, "All fields are required");
-    }
+//     if (!email || !password || !confirmPassword || !fullName || !dob || !mobileNumber || !gender) {
+//         throw new ApiError(400, "All fields are required");
+//     }
 
-    const existedUser = await User.findOne({ email });
-    if (existedUser) {
-        throw new ApiError(409, "User already exists");
-    }
+//     const existedUser = await User.findOne({ email });
+//     if (existedUser) {
+//         throw new ApiError(409, "User already exists");
+//     }
 
-    if (password !== confirmPassword) {
-        throw new ApiError(400, "Passwords do not match");
-    }
+//     if (password !== confirmPassword) {
+//         throw new ApiError(400, "Passwords do not match");
+//     }
 
-    // CHECK VERIFIED EMAIL
-    const otpRecord = await Otp.findOne({ email });
+//     // CHECK VERIFIED EMAIL
+//     const otpRecord = await Otp.findOne({ email });
 
-    if (!otpRecord || !otpRecord.isVerified) {
-        throw new ApiError(403, "Please verify your email first");
-    }
+//     if (!otpRecord || !otpRecord.isVerified) {
+//         throw new ApiError(403, "Please verify your email first");
+//     }
 
-    // profile upload
-    const profilePictureLocalPath = req.files?.profilePicture?.[0]?.path;
+//     // profile upload
+//     const profilePictureLocalPath = req.files?.profilePicture?.[0]?.path;
 
-    let profilePictureLiveURL;
-    if (profilePictureLocalPath) {
-        const profilePicture = await uploadOnCloudinary(profilePictureLocalPath);
-        profilePictureLiveURL = profilePicture?.url;
-    }
+//     let profilePictureLiveURL;
+//     if (profilePictureLocalPath) {
+//         const profilePicture = await uploadOnCloudinary(profilePictureLocalPath);
+//         profilePictureLiveURL = profilePicture?.url;
+//     }
 
-    const user = await User.create({
-        email,
-        password,
-        fullName,
-        dob,
-        mobileNumber,
-        gender,
-        profilePicture: profilePictureLiveURL,
-        authProvider: "email",
-        isVerified: true
-    });
+//     const user = await User.create({
+//         email,
+//         password,
+//         fullName,
+//         dob,
+//         mobileNumber,
+//         gender,
+//         profilePicture: profilePictureLiveURL,
+//         authProvider: "email",
+//         isVerified: true
+//     });
 
 
-    const accessToken = createAccessToken(user);
+//     const accessToken = createAccessToken(user);
 
-    const { token: refreshToken, id: refreshId } = await createRefreshToken({
-        userId: user._id,
-        ip: req.ip,
-        userAgent: req.headers["user-agent"],
-    });
+//     const { token: refreshToken, id: refreshId } = await createRefreshToken({
+//         userId: user._id,
+//         ip: req.ip,
+//         userAgent: req.headers["user-agent"],
+//     });
 
-    const isProd = process.env.NODE_ENV === "production";
+//     const isProd = process.env.NODE_ENV === "production";
 
-    const cookieOptions = {
-        httpOnly: true,
-        secure: isProd,
-        sameSite: isProd ? "None" : "Lax",
-        path: "/",
-    };
+//     const cookieOptions = {
+//         httpOnly: true,
+//         secure: isProd,
+//         sameSite: isProd ? "None" : "Lax",
+//         path: "/",
+//     };
 
-    res.cookie("access_token", accessToken, {
-        ...cookieOptions,
-        maxAge: 2 * 60 * 60 * 1000,
-    });
+//     res.cookie("access_token", accessToken, {
+//         ...cookieOptions,
+//         maxAge: 2 * 60 * 60 * 1000,
+//     });
 
-    res.cookie("refresh_token", refreshToken, {
-        ...cookieOptions,
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-    });
+//     res.cookie("refresh_token", refreshToken, {
+//         ...cookieOptions,
+//         maxAge: 30 * 24 * 60 * 60 * 1000,
+//     });
 
-    res.cookie("refresh_token_id", refreshId.toString(), {
-        ...cookieOptions,
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-    });
+//     res.cookie("refresh_token_id", refreshId.toString(), {
+//         ...cookieOptions,
+//         maxAge: 30 * 24 * 60 * 60 * 1000,
+//     });
 
-    // delete OTP after success
-    await Otp.deleteMany({ email });
+//     // delete OTP after success
+//     await Otp.deleteMany({ email });
 
-    return res
-        .status(201)
-        .json(
-            new ApiResponse(201,
-                {
-                    _id: user._id,
-                    email: user.email,
-                    fullName: user.fullName,
-                },
-                "User registered successfully")
-        )
-});
+//     return res
+//         .status(201)
+//         .json(
+//             new ApiResponse(201,
+//                 {
+//                     _id: user._id,
+//                     email: user.email,
+//                     fullName: user.fullName,
+//                 },
+//                 "User registered successfully")
+//         )
+// });
 
 
 export const editProfile = asyncHandler(async (req, res) => {
